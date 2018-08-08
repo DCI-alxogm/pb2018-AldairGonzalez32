5034a45fd782a3977ab8726e72fe650d75b556ad
Aldair Gonzalez <gonzalezvu2018@licifug.ugto.mx>
2018-08-08 09:25:42 -0500

    Cree mi primer programa.

---
+++ semana_1/programa_hola.c
@@ -0,0 +1,15 @@
+/*
+Esto es un comentario. Me sirve para escribir la informacion relevante del programa que estoy haciendo. Por ejemplo:
+Este programa imprime la típica frase "hola mundo a la pantalla"
+Creado el 06/08/18 por Aldair González
+*/
+/*Creado el 8 de agosto de 2018 por Aldair Gonzalez */
+
+#include <stdio.h>
+int main()
+{
+	printf("Hola Aldair\n");
+	printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
+
