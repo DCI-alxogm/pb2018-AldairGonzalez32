8c376fb913d7f8a457bf123a16d63877c4c04900
Aldair Gonzalez <gonzalezvu2018@licifug.ugto.mx>
2018-08-15 08:57:56 -0500

    Agregue la carpeta semana_2

---
+++ primer_programa.c
@@ -0,0 +1,12 @@
+/* Creado el 13 de agosto de 2018 por Aldair Gonzalez */
+
+#include<stdio.h>
+
+int main ()
+{
+     int edad;
+     printf("Este es el primer programa del curso");
+     printf("18: \n");
+     scanf("%i" &edad);
+     printf("/n Tu edad es: \n", &edad);
+return(0)
---
+++ semana_2/progama_edad.c
@@ -0,0 +1,15 @@
+/*Creado el 13 de agosto de 2018 por Aldair Gonzalez*/
+
+#include<stdio.h>
+
+int main ()
+{ 
+    int edad;
+
+    printf("Hola Aldair \n");
+    printf("Este es el 1er programa del curso");
+    printf("Introduce tu edad: \n");
+    scanf("%i", &edad);
+    printf("\nTu edad es: %i \n", edad);
+    return 0;
+}
