bcbefc81c5e2526b36f32fb4afdf56cf50eeded7
Aldair Gonzalez <gonzalezvu2018@licifug.ugto.mx>
2018-09-10 09:56:37 -0500

    Comence el primer ejercicio.

---
+++ semana6/Primer_ejercicio.c
@@ -0,0 +1,16 @@
+/*Programa creado por Aldair Gonzalez el 10 de septiembre de 2018.
+Este programa solicita informacion de un numero pre-determinado de estudiantes. Ej. que pida la edad, el sexo (0:hombre, 1:mujer), semestre (1-9) y promedio de la carrera de 10 alumnos.*/
+
+#include<stdio.h>
+
+int main(){
+
+   int n, i;
+   float est[n];
+
+   printf("Introduzca el número de estudiantes");
+   Scanf("%i\n", &n);
+   printf("Introduce sus calificaciones en orden");
+   for(i=0;i<N;i++){
+      scanf("%f", &est[i]);
+      }
