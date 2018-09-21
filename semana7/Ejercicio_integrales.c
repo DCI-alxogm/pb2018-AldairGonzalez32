ec2aadb05246ab575f524c5c9e54f3b1e31543ce
Aldair Gonzalez <gonzalezvu2018@licifug.ugto.mx>
2018-09-21 11:56:21 -0500

    Termine el programa de integrales.

--- semana7/Ejercicio_integrales.c
+++ semana7/Ejercicio_integrales.c
@@ -9,24 +9,24 @@ Se guardan los resultados en otro archivo externo, en la primera columna el valo
 int main(){
 
     FILE*lec;
-    lec=fopen("Integral_variable.txt", "r"); //Aqui abro el archivo de lectura de donde sacare algunas variables.
-    int p, in, x, N, i, k, y, h; //Aquí meto las variables que usare, p es la potencia, in es el inicio del intervalo a evaluar, x es la variable en que se evaluaran las funciones y el termino de mi intervalo, N es el número de particiones en la función, i es para los for, k son las divisiones para la integral, y es para ayudar en el cálculo de la sumatoria al igual que h.
-    float sum, f, I; //Estas variables me ayudaran para calcular la sumatoria de la integral e imprimir f(x) y F(x)
+    lec=fopen("Integral_variables.txt", "r"); //Aqui abro el archivo de lectura de donde sacare algunas variables.
+    int p, N, i, k; //Aquí meto las variables que usare, p es la potencia, in es el inicio del intervalo a evaluar, x es la variable en que se evaluaran las funciones y el termino de mi intervalo, N es el número de particiones en la función, i es para los for, k son las divisiones para la integral, y es para ayudar en el cálculo de la sumatoria al igual que h.
+    float sum=0, f, I, in, x, h, y=0; //Estas variables me ayudaran para calcular la sumatoria de la integral e imprimir f(x) y F(x)
 
-    fscanf(lec, "%i %i %i %i %i", &p, &in, &x, &N, &k); 
+    fscanf(lec, "%i %f %f %i %i", &p, &in, &x, &N, &k); 
     
     FILE*esc;
     esc=fopen("Resultados.txt", "w"); //aqui abro el archivo donde se escribiran los resultados.
-    h=(x-in)/k;    //Aquí declaro la variable h, necesaria para hacer la integral.
-    for(i=1; i<=k; i++){
-    y=pow((in+i*h), p);
-    sum=y+sum;}  //Aquí hago la sumatoria necesaria para el método de integración.
+    h=(x-in)/N;    //Aquí declaro la variable h, necesaria para hacer la integral.
+    for(i=0; i<=N; i++){
+    y=pow((in+(i*h)),p);
+    sum=sum+y;}  //Aquí hago la sumatoria necesaria para el método de integración.
 
     for(i=in; i<=x; i++){
     fprintf(esc, "El valor de x es %i \n", i);
     f=pow(i, p);
     fprintf(esc, "El valor de f(x) es %f \n", f);
-    I=((in+x)/2)+sum;
+    I=h*(((i+x)/2)+sum);
     fprintf(esc, "El valor de F(x) es %f \n", I);
     } //En este for hice la impresión de resultados de i, f(x) y F(x)
     fclose(lec);
