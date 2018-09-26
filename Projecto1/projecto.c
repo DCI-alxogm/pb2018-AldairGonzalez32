68e24d368f6f4a5d959dcfc1b77a0f71137f7b46
Aldair Gonzalez <gonzalezvu2018@licifug.ugto.mx>
2018-09-26 13:39:38 -0500

    Avance en el projecto parcial.

--- Projecto1/projecto.c
+++ Projecto1/projecto.c
@@ -5,19 +5,38 @@
 
 int main(){
 
-    double xo, yo, zo, vxo, vyo, vzo, xi, yi, zi, vix, viy, viz; //Aquí se declaran las variables que se utilizaran, los valores de las posiciones y velocidades iniciales y consecuentes, ademas del tiempo de evolución y lo necesario para calcularlo.
-    float to=0, ti, h, tf;
-    FILE*Dat;                               //Aquí se abre el archivo de donde se sacaran las variables.
+    double xo, yo, zo, vxo, vyo, vzo, xi, yi, zi, vix, viy, viz, r, i, h; //Aquí se declaran las variables que se utilizaran, los valores de las posiciones y velocidades iniciales y consecuentes, ademas del tiempo de evolución y lo necesario para calcularlo.
+    float to=0, tf;
+    float G=4*3.141592, M=1;
+    FILE*Dat;
+    FILE*Res;                               //Aquí se abre el archivo de donde se sacaran las variables.
     
     Dat=fopen("Datos.txt", "r");
-    fscanf(Dat, "%lf %lf %lf %lf %lf %lf %f", &xo, &yo, &zo, &vxo, &vyo, &vzo, &h);  //Aquí me aseguro que este tomando los valores indicados para realizar el cálculo de la orbita.
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
+    fscanf(Dat, "%lf %lf %lf %lf %lf %lf %lf %f", &xo, &yo, &zo, &vxo, &vyo, &vzo, &h, &tf);  //Aquí me aseguro que este tomando los valores indicados para realizar el cálculo de la orbita.
+    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ, h es %f y tf es %f\n", xo, yo, zo, vxo, vyo, vzo, h, tf);
+    
+    r=sqrt(pow(xo,2)+pow(yo,2)+pow(zo,2)); //Aqui doy valor a "r", variable necesaria para las ecuaciones.
 
-    for(i=t0; i<tf; i+h){
-    xi=xo+vxo*h
-    yi=yo+vyo*h
-    zi=xo+vzo*h
-    voi=vox-(h*(G*M*xo)/r
+    fclose(Dat);  
+
+    Res=fopen("Resultados.txt", "w");
+
+    for(i=to; i<tf; i+=h){   //Aqui se hacen los calculos para la trayectoria de los planetas.
+    xi=xo+vxo*h;
+    yi=yo+vyo*h;
+    zi=xo+vzo*h;
+    vix=vxo-(h*((G*M*xo)/pow(r,3)));
+    viy=vyo-(h*((G*M*yo)/pow(r,3)));
+    viz=vzo-(h*((G*M*zo)/pow(r,3)));
+    fprintf(Res,"%f %f %f \n", xi, yi, zi);
+    xo=xi;
+    yo=yi;
+    zo=zi;
+    vxo=vix;
+    vyo=viy;
+    vzo=viz;
+    }
+    fclose(Res);
 
 return 0;
 }
