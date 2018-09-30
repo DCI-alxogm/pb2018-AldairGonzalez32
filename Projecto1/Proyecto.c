4501676ec7daae5f53e95c49c882a32b8ada7f15
Aldair Gonzalez <aldairgonzalez@localhost.localdomain>
2018-09-30 01:30:02 -0500

    Agregue el codigo del programa.

--- Projecto1/Proyecto.c
+++ Projecto1/Proyecto.c
@@ -5,113 +5,103 @@
 
 int main(){
 
-    double xo, yo, zo, vxo, vyo, vzo, xi, yi, zi, vix, viy, viz, r, i, h; 
-    float to=0, tf;
-    float G=4*3.141592*3.141592, M=1;
-    int opcion;     //Aquí se declaran las variables que se utilizaran, los valores de las posiciones y velocidades iniciales y consecuentes, ademas del tiempo de evolución y lo necesario para calcularlo.
-    FILE*Par;
+    double xo, yo, zo, vxo, vyo, vzo, r, i, h; 
+    float tf;
+    float G, M=1;
+    int p=1;     //Aquí se declaran las variables que se utilizaran, los valores de las posiciones y velocidades iniciales y consecuentes, ademas del tiempo de evolución y lo necesario para calcularlo.
+    
     FILE*Var;
-    FILE*Res;       //Aquí se hacen las variables para los archivos que se usaran en el prograna, el archivo Var dependera del planeta escogido, de donde tomara entre 9 ficheros para escoger.
+    FILE*Res;       //Aqui se hacen las variables de los ficheros necesarios para el programa
     
     printf("Escoge el planeta para el cual se hara la simulacion de orbita. \n(1) Mercurio \n(2) Venus \n(3) Tierra \n(4) Marte \n(5) Jupiter \n(6) Saturno \n(7) Urano \n(8) Neptuno \n(9) Pluton. ");
-    scanf("%d", &opcion);
-    Par=fopen("Parametros.txt", "r"); 
-    fscanf(Par, "%lf %f", &h, &tf);       //Aquí se toman los valores de intervalo y tiempo final de la orbita de un archivo unicamente para eso, esto pensado para poder modificar los parametros sin problemas.
-    printf("El intervalo entre cada evaluación es %f y el final de la evaluación es %f\n", h, tf);
-    
-    fclose(Par);  
-    
-    switch(opcion){   //Este switch es para escoger la orbita que sera evaluada, se tomaran las distintas variables necesarias para cada uno.
-    case 1:
-    Var=fopen("Mercurio.txt", "r");
+    Var=fopen("Datos.txt", "r");
+    fscanf(Var, "%lf %f", &h, &tf);       //Aquí se toman los valores de intervalo y tiempo final de la orbita.
+    printf("El intervalo entre cada evaluación es %f y el final de la evaluación es %f\n", h, tf); //imprimo el valor de h y tf para asegurarme de que funcionan.
+        
+    for(p=1;p<10;p++){ //Este for se usa para dar distintos valores de switch, y así realizar todos los casos, uno tras otro, esto también permite modificar el programa facilmente para graficar una sola orbita a elección del usuario.
+
+    switch(p){   //Este switch es para escoger la orbita que sera evaluada, se tomaran las distintas variables necesarias para cada uno.
+    case 1:     //Cada caso dara valores distintos a las variables de posición y velocidad inicial para hacer la evaluación de cada planeta.
+    Res=fopen("Mercurio.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
+    printf("Valores de Mercurio\n Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ\n", xo, yo, zo, vxo, vyo, vzo);
     break;
 
     case 2:
-    Var=fopen("Venus.txt", "r");
+    Res=fopen("Venus.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
+    printf("Valores de Venus \n Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ\n", xo, yo, zo, vxo, vyo, vzo);
     break;
 
     case 3:
-    Var=fopen("Tierra.txt", "r");
+    Res=fopen("Tierra.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
+    printf("Valores de Tierra\n Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ\n", xo, yo, zo, vxo, vyo, vzo);
     break;
 
     case 4:
-    Var=fopen("Marte.txt", "r");
+    Res=fopen("Marte.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
+    printf("Valores de Marte\n Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ\n ", xo, yo, zo, vxo, vyo, vzo);
+  
     break;
 
     case 5:
-    Var=fopen("Jupiter.txt", "r");
+    Res=fopen("Jupiter.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
+    printf("Valores de jupiter\n Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ\n", xo, yo, zo, vxo, vyo, vzo);
+    
     break;
 
     case 6:
-    Var=fopen("Saturno.txt", "r");
+    Res=fopen("Saturno.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
+    printf("Valores de Jupiter\n Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ \n", xo, yo, zo, vxo, vyo, vzo);
+    
     break;
 
     case 7:
-    Var=fopen("Urano.txt", "r");
+    Res=fopen("Urano.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
+    printf("Valores de Urano\n Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ\n", xo, yo, zo, vxo, vyo, vzo);
+    
     break;
 
     case 8:
-    Var=fopen("Neptuno.txt", "r");
+    Res=fopen("Neptuno.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
+    printf("Valores de Neptuno\n Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ\n", xo, yo, zo, vxo, vyo, vzo);
+    
     break;
 
     case 9:
-    Var=fopen("Pluton.txt", "r");
+    Res=fopen("Pluton.txt", "w");
     fscanf(Var, "%lf %lf %lf %lf %lf %lf", &xo, &yo, &zo, &vxo, &vyo, &vzo);
-    printf("Posición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ", xo, yo, zo, vxo, vyo, vzo);
-    fclose(Var);
-    break;
-
-    default:
-    printf("El sol y la luna no son planetas, escoge un planeta de la lista");
-    scanf("%i", &opcion);
+    printf("Valores de Pluton\nPosición inicial: (%f, %f, %f) velocidad inicial %f î + %f ĵ + %f ẑ\n", xo, yo, zo, vxo, vyo, vzo);
+    
     break;
 
     }
+    r=sqrt((xo*xo)+(yo*yo)+(zo*zo));
+    G=4.0*pow(3.14159265359,2);
+    vxo*=365.242;
+    vyo*=365.242;
+    vzo*=365.242;
 
-    r=sqrt(pow(xo,2)+pow(yo,2)+pow(zo,2)); //Aqui doy valor a "r", variable necesaria para las ecuaciones.
-
-    Res=fopen("Resultados.txt", "w");
-
-    for(i=to; i<tf; i+=h){   //Aqui se hacen los calculos para la trayectoria de los planetas, usando una función ciclica que nos da los cambios poco a poco y los escribe en un archivo llamado Resultados.
-    xi=xo+vxo*h;
-    yi=yo+vyo*h;
-    zi=xo+vzo*h;
-    vix=vxo-(h*((G*M*xo)/pow(r,3)));
-    viy=vyo-(h*((G*M*yo)/pow(r,3)));
-    viz=vzo-(h*((G*M*zo)/pow(r,3)));
-    fprintf(Res,"%f %f %f \n", xi, yi, zi);
-    xo=xi;
-    yo=yi;
-    zo=zi;
-    vxo=vix;
-    vyo=viy;
-    vzo=viz;
+    for(i=0; i<tf; i+=h){   //Aqui se hacen los calculos para la trayectoria de los planetas, usando una función ciclica que nos da los cambios con un intervalo h, escribiendo los resultados en distintos archivos para cada planeta evaluado.
+    
+    xo=xo+vxo*h;
+    yo=yo+vyo*h;
+    zo=zo+vzo*h;
+    
+    vxo=vxo-(h*((G*M*xo)/pow(r,3)));
+    vyo=vyo-(h*((G*M*yo)/pow(r,3)));
+    vzo=vzo-(h*((G*M*zo)/pow(r,3)));
+    fprintf(Res,"%f %f %f %f %f %f \n", xo, yo, zo, vxo, vyo, vzo);
+    
     }
-    fclose(Res);
-
+   
+    fclose(Res);  //Por ultimo, cierro el fichero donde escribí los datos.
+    }
 return 0;
 }
