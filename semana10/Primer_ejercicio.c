9b98ef0d4d5fd3569a5014e5b4b70be13b4a9a12
Aldair Gonzalez <gonzalezvu2018@licifug.ugto.mx>
2018-10-15 09:41:26 -0500

    Comence semana 10

---
+++ semana10/Primer_ejercicio.c
@@ -0,0 +1,11 @@
+/*Hacer un programa que genere una secuencia de coordenadas x,y,z, que formen un arreglo de puntos igualmente espaciados en las 3 direcciones. Dar desde la linea de comandos: nombre del archivo donde se guardaran los datos, numero de puntos en el arreglo (debe ser un numero de la fomra n³ <<3 dimensiones >>), y la separación entre los puntos (o bien, la distancia mas grande entre los puntos en una dimensión, 1 lado del cubo)*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main( char *let[], int n, d){
+
+        char *nombre_archivo;
+        int n, i;
+        nombre_archivo=let[1];
+        n=i;
